3374e22170f26f863e291dd0098f52e734667cff
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-29 08:43:51 +0800

    将`settings.py`中，对`SECRET_KEY`的`硬编码`（位数太少，`不安全`）改为项目自身的`SECRET_KEY`。

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -160,8 +160,7 @@ JWT_AUTH = {
     # 'JWT_RESPONSE_PAYLOAD_HANDLER':
     # 'rest_framework_jwt.utils.jwt_response_payload_handler',
 
-    # 'JWT_SECRET_KEY': settings.SECRET_KEY,
-    'JWT_SECRET_KEY': 'yantai2018',
+    'JWT_SECRET_KEY': settings.SECRET_KEY,
     # 'JWT_GET_USER_SECRET_KEY': None,
     # 'JWT_PUBLIC_KEY': None,
     # 'JWT_PRIVATE_KEY': None,
